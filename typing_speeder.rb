75c5ee29cbe8aa87cffc04c6bda455df7b0f74ce
kostek <ka.koszewski@gmail.com>
2023-12-10 21:02:22 +0100

    up

--- typing_speeder.rb
+++ typing_speeder.rb
@@ -1,5 +1,6 @@
 require 'ruby2d'
-word_list = IO.readlines('./words.txt').map(&:strip)
+require 'byebug'
+possible_word_list = IO.readlines('./words.txt').map(&:strip)
 
 WIDTH = 600
 HEIGHT = 480
@@ -11,16 +12,17 @@ set background: 'gray'
 set title: "typing speeder"
 
 
-def draw_bottom_panel(current_word, seconds, lives)
+def draw_bottom_panel(current_word, seconds, lives, words_typed)
   Rectangle.new(x: 0, y: HEIGHT - 100, width: WIDTH, height: 100, color: 'blue')
   Line.new(x1: 200, x2: 200, y1: HEIGHT - 100, y2: HEIGHT, color: 'white')
   Text.new('Press ESC to pause', x: 35, y: HEIGHT - 35, color: 'white', size: 15)
 
   # left top side panel 
   Text.new("time: #{seconds}", x: 20, y: HEIGHT - 90, color: 'white', size: 15)
-  Text.new("lives: #{lives}",  x: 60, y: HEIGHT - 90, color: 'white', size: 15)
+  Text.new("lives: #{lives}",  x: 120, y: HEIGHT - 90, color: 'white', size: 15)
   # left bottom side panel 
-  Text.new('words:', x: 20, y: HEIGHT - 65, color: 'white', size: 15)
+  Text.new("words: #{words_typed}", x: 20, y: HEIGHT - 65, color: 'white', size: 15)
+  Text.new("wpm: #{0}",  x: 120, y: HEIGHT - 65, color: 'white', size: 15)
 
   # right side panel 
   Text.new(current_word, x: (WIDTH - 200) / 2 + 20 , y: HEIGHT - 90, color: 'white', size: 50)
@@ -30,13 +32,37 @@ def draw_pause_view
   Text.new('PAUSED', x: WIDTH / 3, y: HEIGHT / 3, size: 45, color: 'red')
 end
 
+def generate_new_word(current_words, possible_word_list)
+  if current_words.empty?
+    return Word.new()
+  end
+
+  occupied_pos = current_words.map {|word| [word.x,word.y]}  
+  x,y = 0,0
+
+  # avoid collision between words 
+  loop do
+    x = WIDTH - rand(50..150)
+    y = rand(0..HEIGHT-120)
+    collision = false
+
+    occupied_pos.each do |pos|
+      collision = true if (pos[0]-10..pos[0]+10).include?(x) && (pos[1]-10..pos[1]+10).include?(y)
+    end 
+    
+    break unless collision
+  end 
+
+  Word.new(possible_word_list.sample, x, y)
+end 
+
 class Word
   attr_reader :x, :y, :content
 
-  def initialize(content)
+  def initialize(content, x, y)
     @content = content
-    @x = WIDTH - rand(50..150)
-    @y = rand(0..HEIGHT-120)
+    @x = x
+    @y = y
   end
   
   def draw
@@ -50,22 +76,23 @@ end
 
 paused = false
 current_word = ''
-words = []
-lives = 0
+current_words = []
+words_typed = 0
+lives = 5
 time_start = Time.new 
 seconds = 0
 seconds_before_pause = 0
 
 # generate couple words to start with 
 5.times do
-  words << Word.new(word_list.sample)
-  words.each(&:draw)
+  current_words << Word.new(possible_word_list.sample, WIDTH - rand(50..150), rand(0..HEIGHT-120))
 end
+generate_new_word(current_words, possible_word_list)
 
 update do
   clear
   
-  draw_bottom_panel(current_word, seconds, lives)
+  draw_bottom_panel(current_word, seconds, lives, words_typed)
 
   if paused
     draw_pause_view
@@ -76,13 +103,13 @@ update do
 
   seconds = seconds_before_pause + (Time.new - time_start).to_i
   
-  words.each(&:draw)
+  current_words.each(&:draw)
 
   # each 3 seconds generate new word 
-  words << Word.new(word_list.sample) if Window.frames % 90 == 0 
+  # current_words << Word.new(word_list.sample) if Window.frames % 90 == 0 
 
   # each 2/3 of second move each words to the left 
-  words.each(&:move) if Window.frames % 20 == 0 
+  current_words.each(&:move) if Window.frames % 20 == 0 
 
 end 
 
