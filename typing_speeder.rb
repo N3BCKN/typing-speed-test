57cdba35c82966a85f9c746f9d1046b542fd7a4f
kostek <ka.koszewski@gmail.com>
2023-12-10 19:38:53 +0100

    move words toward left side of the screen

--- typing_speeder.rb
+++ typing_speeder.rb
@@ -1,40 +1,91 @@
 require 'ruby2d'
+word_list = IO.readlines('./words.txt').map(&:strip)
 
 WIDTH = 600
 HEIGHT = 480
 
 set width: WIDTH
 set height: HEIGHT
+set fps_cap: 30
 set background: 'gray'
 set title: "typing speeder"
 
-def draw_bottom_panel(current_word)
+
+def draw_bottom_panel(current_word, seconds)
   Rectangle.new(x: 0, y: HEIGHT - 100, width: WIDTH, height: 100, color: 'blue')
   Line.new(x1: 200, x2: 200, y1: HEIGHT - 100, y2: HEIGHT, color: 'white')
   Text.new('Press ESC to pause', x: 35, y: HEIGHT - 35, color: 'white', size: 15)
 
+  Text.new("time: #{seconds}",  x: 20, y: HEIGHT - 90, color: 'white', size: 15)
+  Text.new('words:', x: 20, y: HEIGHT - 65, color: 'white', size: 15)
+
   Text.new(current_word, x: (WIDTH - 200) / 2 + 20 , y: HEIGHT - 90, color: 'white', size: 50)
 end 
 
-def draw_pause
+def draw_pause_view
   Text.new('PAUSED', x: WIDTH / 3, y: HEIGHT / 3, size: 45, color: 'red')
+end
+
+class Word
+  attr_reader :x, :y, :content
+
+  def initialize(content)
+    @content = content
+    @x = WIDTH - rand(50..150)
+    @y = rand(0..HEIGHT-120)
+  end
+  
+  def draw
+    Text.new(@content, x: @x, y: @y, size: 20, color: 'blue')
+  end
+
+  def move
+    @x -= 5
+  end
 end
 
 paused = false
 current_word = ''
+words = []
+lives = 0
+time_start = Time.new 
+seconds = 0
+seconds_before_pause = 0
+
+# generate couple words to start with 
+5.times do
+  words << Word.new(word_list.sample)
+  words.each(&:draw)
+end
 
 update do
   clear
+  
+  draw_bottom_panel(current_word, seconds)
 
-  draw_bottom_panel(current_word)
   if paused
-    draw_pause
+    draw_pause_view
+    seconds_before_pause = seconds
+    time_start = Time.new
     next 
   end 
+
+  seconds = seconds_before_pause + (Time.new - time_start).to_i
+  
+
+  words.each(&:draw)
+
+  # each 3 seconds generate new word 
+  words << Word.new(word_list.sample) if Window.frames % 90 == 0 
+
+  # each 2/3 of second move each words to the left 
+  words.each(&:move) if Window.frames % 20 == 0 
+
 end 
 
 on :key_down do |event|
   paused = !paused if event.key == 'escape'
+  next if paused
 
   if ('a'..'z').to_a.include?(event.key)
     current_word += event.key.upcase unless current_word.size > 15
