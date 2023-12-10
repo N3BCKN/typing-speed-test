ab9aa90b2206261ed25736fe725e4fca1f48f733
kostek <ka.koszewski@gmail.com>
2023-12-10 22:13:34 +0100

    cleaning

--- typing_speeder.rb
+++ typing_speeder.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 require 'ruby2d'
-require 'byebug'
 possible_word_list = IO.readlines('./words.txt').map(&:strip)
 
 WIDTH = 600
@@ -9,52 +10,52 @@ set width: WIDTH
 set height: HEIGHT
 set fps_cap: 30
 set background: 'gray'
-set title: "typing speeder"
-
+set title: 'typing speeder'
 
 def draw_bottom_panel(current_word, seconds, lives, words_typed)
+  wpm = seconds.zero? ? 0 : (words_typed / (seconds / 60.0)).truncate(2)
+
   Rectangle.new(x: 0, y: HEIGHT - 100, width: WIDTH, height: 100, color: 'blue')
   Line.new(x1: 200, x2: 200, y1: HEIGHT - 100, y2: HEIGHT, color: 'white')
   Text.new('Press ESC to pause', x: 35, y: HEIGHT - 35, color: 'white', size: 15)
 
-  # left top side panel 
+  # left top side panel
   Text.new("time: #{seconds}", x: 20, y: HEIGHT - 90, color: 'white', size: 15)
   Text.new("lives: #{lives}",  x: 120, y: HEIGHT - 90, color: 'white', size: 15)
-  # left bottom side panel 
+  # left bottom side panel
   Text.new("words: #{words_typed}", x: 20, y: HEIGHT - 65, color: 'white', size: 15)
-  Text.new("wpm: #{0}",  x: 120, y: HEIGHT - 65, color: 'white', size: 15)
+  Text.new("wpm: #{wpm}", x: 120, y: HEIGHT - 65, color: 'white', size: 15)
 
-  # right side panel 
-  Text.new(current_word, x: (WIDTH - 200) / 2 + 20 , y: HEIGHT - 90, color: 'white', size: 50)
-end 
+  # right side panel
+  Text.new(current_word, x: (WIDTH - 200) / 2 + 20, y: HEIGHT - 90, color: 'white', size: 50)
+end
 
-def draw_pause_view
-  Text.new('PAUSED', x: WIDTH / 3, y: HEIGHT / 3, size: 45, color: 'red')
+def draw_break_view(text)
+  Text.new(text, x: WIDTH / 3 - 10, y: HEIGHT / 3, size: 45, color: 'red')
 end
 
 def generate_new_word(current_words, possible_word_list)
-  if current_words.empty?
-    return Word.new()
-  end
+  return Word.new(possible_word_list.sample, WIDTH - rand(50..170), rand(0..HEIGHT - 120)) if current_words.empty?
 
-  occupied_pos = current_words.map {|word| [word.x,word.y]}  
-  x,y = 0,0
+  occupied_pos = current_words.map { |word| [word.x, word.y] }
+  x = 0
+  y = 0
 
-  # avoid collision between words 
+  # avoid collision between words
   loop do
     x = WIDTH - rand(50..150)
-    y = rand(0..HEIGHT-120)
+    y = rand(0..HEIGHT - 120)
     collision = false
 
     occupied_pos.each do |pos|
-      collision = true if (pos[0]-10..pos[0]+10).include?(x) && (pos[1]-10..pos[1]+10).include?(y)
-    end 
-    
+      collision = true if (pos[0] - 25..pos[0] + 25).include?(x) && (pos[1] - 20..pos[1] + 20).include?(y)
+    end
+
     break unless collision
-  end 
+  end
 
   Word.new(possible_word_list.sample, x, y)
-end 
+end
 
 class Word
   attr_reader :x, :y, :content
@@ -64,7 +65,7 @@ class Word
     @x = x
     @y = y
   end
-  
+
   def draw
     Text.new(@content, x: @x, y: @y, size: 20, color: 'blue')
   end
@@ -79,55 +80,68 @@ current_word = ''
 current_words = []
 words_typed = 0
 lives = 5
-time_start = Time.new 
+time_start = Time.new
 seconds = 0
 seconds_before_pause = 0
 
-# generate couple words to start with 
+# generate couple words to start with
 5.times do
   current_words << generate_new_word(current_words, possible_word_list)
 end
 
-
 update do
   clear
-  
+
   draw_bottom_panel(current_word, seconds, lives, words_typed)
 
+  if lives <= 0
+    draw_break_view('GAME OVER')
+    next
+  end
+
   if paused
-    draw_pause_view
+    draw_break_view('PAUSED')
     seconds_before_pause = seconds
     time_start = Time.new
-    next 
-  end 
+    next
+  end
 
   seconds = seconds_before_pause + (Time.new - time_start).to_i
-  
+
   current_words.each(&:draw)
 
-  # each 3 seconds generate new word 
-  current_words << generate_new_word(current_words, possible_word_list) if Window.frames % 90 == 0 
+  # each 1/2 of second move each words to the left
+  current_words.each(&:move) if (Window.frames % 15).zero?
 
-  # each 2/3 of second move each words to the left 
-  current_words.each(&:move) if Window.frames % 20 == 0 
+  # check if any word reached far left edge of the screen
+  current_words.each_with_index do |word, index|
+    if word.x <= 0
+      lives -= 1
+      current_words.delete_at(index)
+    end
+  end
 
-end 
+  # each 1.5 seconds generate new word
+  current_words << generate_new_word(current_words, possible_word_list) if (Window.frames % 45).zero?
+end
 
 on :key_down do |event|
   paused = !paused if event.key == 'escape'
   next if paused
 
-  if ('a'..'z').to_a.include?(event.key)
-    current_word += event.key.upcase unless current_word.size > 15
-  end 
+  current_word += event.key if ('a'..'z').include?(event.key) && current_word.size <= 15
 
-  if event.key == 'backspace'
-    current_word = current_word.chop
-  end 
+  current_word = current_word.chop if event.key == 'backspace'
 
   if event.key == 'return'
+    current_words.each_with_index do |word, index|
+      if word.content == current_word
+        current_words.delete_at(index)
+        words_typed += 1
+      end
+    end
     current_word = ''
-  end 
+  end
 end
 
 show
